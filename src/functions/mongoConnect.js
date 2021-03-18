48ffad37962cff814a0298a5577464168ebf645f
seekeroftacos <theoneandonlyseek@gmail.com>
2021-03-17 21:44:09 -0500

    having fun

--- src/functions/mongoConnect.js
+++ src/functions/mongoConnect.js
@@ -5,7 +5,6 @@ module.exports = {
   connect: async () => {
     mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log('Initializing database connection...');
