be54cc41ee7b5a3f122cffa0bc1c9c0b3660accf
seekeroftacos <theoneandonlyseek@gmail.com>
2021-05-28 16:45:24 -0500

    hosting?

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  approval: '821601823320047646',
+  approval: '847144413548511252',
   ytcID: '677594854653558834',
-  staffChannel: '821626849787379712',
-  ytcBumpChannel: '821192580704108544',
+  staffChannel: '847144613193449472',
+  ytcBumpChannel: '847144620080496710',
 };
